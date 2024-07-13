e1cd45edbe73611eb0ee1216496e3ce81235f7e8
Mevan Manilka <Mmanilka@outlook.com>
2024-07-13 15:10:35 +0530

    Table regenerate

---
+++ DogCareFormApp/DogTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Dog (
+    
+    DogName NVARCHAR(100),
+    DogBreed NVARCHAR(50),
+	DogAge INT,
+	DogGender NVARCHAR(10),
+    
+);
---
+++ DogCareFormApp/OwnerTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE DogOwner (
+    OwnerID INT PRIMARY KEY IDENTITY,
+    OwnerName NVARCHAR(100),
+    OwnerContact NVARCHAR(50),
+	OwnerAddress NVARCHAR(100),
+	OwnerEmail NVARCHAR(100)
+);

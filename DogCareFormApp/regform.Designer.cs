7d2624ec5905307bbdecaacb2a6ccd144f38f127
Mevan Manilka <Mmanilka@outlook.com>
2024-07-18 21:00:56 +0530

    Remove `button4` from regform.Designer.cs
    Removed the declaration, initialization, and settings of `button4` from `regform.Designer.cs`, including its addition to the form's controls. This change means `button4` will no longer be displayed or interactable within the form.

--- DogCareFormApp/regform.Designer.cs
+++ DogCareFormApp/regform.Designer.cs
@@ -52,7 +52,6 @@
             this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblregsiterform
@@ -385,22 +384,11 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(441, 420);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(98, 30);
-            this.button4.TabIndex = 34;
-            this.button4.Text = "Register";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // Regform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(980, 870);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.OwnerEmail);
@@ -459,6 +447,5 @@
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
